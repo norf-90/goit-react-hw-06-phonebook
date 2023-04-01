55de43b955cb1e6deda979e3b24e8470dff0bed3
Oleksandr Shmatko <blacknorfolk@gmail.com>
2023-04-01 14:00:39 +0300

    fix background

--- src/components/GlobalStyles.jsx
+++ src/components/GlobalStyles.jsx
@@ -6,7 +6,7 @@ export const GlobalStyles = () => injectGlobal`
     background-image: url('https://images.unsplash.com/photo-1611416517780-eff3a13b0359?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1949&q=80');
     background-size: auto 100vh;
     background-repeat: no-repeat;
-   background-attachment: ;
+   background-attachment: fixed ;
 
     
     display:flex;
